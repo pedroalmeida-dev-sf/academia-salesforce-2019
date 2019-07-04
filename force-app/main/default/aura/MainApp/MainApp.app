276476c414d836736bd6eefcb3205e5809418fca
Pedro Almeida <pedroalmeida@loba.pt>
2019-07-04 18:21:29 +0100

    added components

---
+++ force-app/main/default/aura/MainApp/MainApp.app
@@ -0,0 +1,3 @@
+<aura:application implements="force:appHostable,flexipage:availableForAllPageTypes,flexipage:availableForRecordHome,force:hasRecordId,forceCommunity:availableForAllPageTypes,force:lightningQuickAction" access="global" controller="ContactController">
+
+</aura:application>	
--- force-app/main/default/aura/componenteContactos/componenteContactosController.js
+++ force-app/main/default/aura/componenteContactos/componenteContactosController.js
@@ -12,5 +12,44 @@
 			}
 		});
 		$A.enqueueAction(contacts);
+	},
+
+	showTabela : function(component,event,helper){
+		component.set("v.showContacts",true);
+	},
+
+	hideTabela : function(component,event,helper){
+		component.set("v.showContacts",false);
+	},
+
+	createContact : function(component,event,helper){
+		var nome = component.find("firstName").get("v.value");
+		var apelido = component.find("lastName").get("v.value");
+		var email = component.find("email").get("v.value");
+
+		var create = component.get("c.createNewContact");
+
+		create.setParams({
+			firstName : nome,
+			lastName : apelido,
+			email : email
+		});
+		create.setCallback(this,function(action){
+			var state = action.getState();
+			if(state === 'SUCCESS'){
+				var response = action.getReturnValue();
+				if(response != null){
+					var toastEvent = $A.get("e.force:showToast");
+					toastEvent.setParams({
+						"title": "Success!",
+						"message": "The record has been created successfully.",
+						"type" : "success"
+					});
+					toastEvent.fire();
+				}
+			}
+		});
+		$A.enqueueAction(create);
 	}
+
 })
